48316038c8be5e6e7bc7fc5a6b8cc519816ea0a3
Roman Jelinek <jelinek82@gmail.com>
2023-02-03 14:52:13 +0100

    Weather changes

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -24,6 +24,8 @@ export default function Weather() {
     }
 
     if (weather != undefined) {
+
+        console.log(weather)
     
         const code: number = weather.current_weather.weathercode;
 
@@ -31,7 +33,7 @@ export default function Weather() {
 
         let days: any = [];
 
-        for (let i = 0; i < 6; i++) {
+        for (let i = 0; i < 7; i++) {
 
             const weekday = ["Pondělí","Úterý","Středa","Čtvrtek","Pátek","Sobota","Neděle"];
             const d = new Date(daily.time[i]);
@@ -51,6 +53,8 @@ export default function Weather() {
                 codeText = "./icons/snowflake.svg";
             } else if (code === 95 || code === 96 || code === 99) {
                 codeText = "./icons/cloud-bolt.svg";
+            } else if (code === 45 || code === 48) {
+                codeText = "./icons/smog.svg";
             }
 
             days.push({
@@ -65,21 +69,20 @@ export default function Weather() {
         
         return (
             <div className={styles.weatherBox}>
-                <h3>Aktuální počasí</h3>
-                <div className={styles.todayBox}>
-                    <div className={styles.today}>
+                <h3>Počasí</h3>
+                <div className={styles.forecastBox}>
+                    <div className={styles.forecast}>
+                        <span>Dnes</span>
                         {(code === 0 || code === 1) && (<span><Image src="./icons/sun.svg" width={50} height={50} alt="sunny"/></span>)}
                         {(code === 2) && (<span><Image src="./icons/cloud-sun.svg" width={50} height={50} alt="partly sunny"/></span>)}
                         {(code === 3) && (<span><Image src="./icons/cloud.svg" width={50} height={50} alt="cloudy"/></span>)}
                         {(code === 51 || code === 53 || code === 55 || code === 56 || code === 57 || code === 61 || code === 63 || code === 65 || code === 66 || code === 67 || code === 80 || code === 81 || code === 82) && (<span><Image src="./icons/cloud-showers-heavy.svg" width={50} height={50} alt="showers"/></span>)}
                         {(code === 71 || code === 73 || code === 75 || code === 77 || code === 85 || code === 86) && (<span><Image src="./icons/snowflake.svg" width={50} height={50} alt="snow"/></span>)}
                         {(code === 95 || code === 96 || code === 99) && (<span><Image src="./icons/cloud-bolt.svg" width={50} height={50} alt="storm"/></span>)}
+                        {(code === 45 || code === 48) && (<span><Image src="./icons/smog.svg" width={50} height={50} alt="fog"/></span>)}
                         <span>teplota: {weather.current_weather.temperature} °C</span> 
                         <span>vítr: {weather.current_weather.windspeed}km/h</span>
                     </div>
-                </div>
-                <h3>Předpověď počasí</h3>
-                <div className={styles.forecastBox}>
                     {days.map((day: any,i: number) => (
                             <div key={i} className={styles.forecast}>
                                 <span>{day.date}</span>
