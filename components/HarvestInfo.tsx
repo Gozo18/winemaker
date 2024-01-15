8643492a6658ee60a0822e56cf6100b47dee222b
Roman Jelinek <jelinek82@gmail.com>
2024-01-15 16:28:31 +0100

    Adv added to harvest

--- components/HarvestInfo.tsx
+++ components/HarvestInfo.tsx
@@ -5,11 +5,11 @@ export default function HarvestInfo({ thisWine }: any) {
   return (
     <div className={styles.outputsBox}>
       {thisWine[0].harvest.length > 0 ? (
-        <>
+        <div className={styles.itemsBox}>
           {thisWine[0].harvest.map((w: any, i: number) => (
             <HarvestInfoItem w={w} wineId={thisWine[0].id} key={i} />
           ))}
-        </>
+        </div>
       ) : (
         <div className={styles.emptyBox}>
           <h4>Žádný záznam!</h4>
