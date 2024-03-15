1fd85156e973c116acbe8d909f55ea151caa43a5
Roman Jelinek <jelinek82@gmail.com>
2024-03-15 17:24:16 +0100

    Ad fix

--- components/Advertising.tsx
+++ components/Advertising.tsx
@@ -14,41 +14,41 @@ export default function Advertising() {
         src="https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v18.0"
         nonce="HGgQb27u"
       ></script>
-      <div className={styles.advBox}>
-        <div className={styles.advImageBox}>
-          <Image
-            src="/winemaker.webp"
-            alt="WineMaker history"
-            fill
-            style={{ objectFit: "contain" }}
-            sizes="(min-width: 320px) 100vw"
-            priority={true}
-          />
-        </div>
-        <div className={styles.advProBox}>
-          <div className={styles.advTextBox}>
-            <p>Líbí se Vám naše aplikace?</p>
-            <p>Prosím, sdílejte!</p>
+      <Link
+        target="_blank"
+        href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwinemaker.cz%2F&amp;src=sdkpreparse"
+        className="fb-xfbml-parse-ignore"
+      >
+        <div className={styles.advBox}>
+          <div className={styles.advImageBox}>
+            <Image
+              src="/winemaker.webp"
+              alt="WineMaker history"
+              fill
+              style={{ objectFit: "contain" }}
+              sizes="(min-width: 320px) 100vw"
+              priority={true}
+            />
           </div>
-          <div className={styles.advButtonBox}>
-            <div
-              className="fb-share-button"
-              data-href="https://winemaker.cz"
-              data-layout=""
-              data-size=""
-            >
-              <Link
-                target="_blank"
-                href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwinemaker.cz%2F&amp;src=sdkpreparse"
-                className="fb-xfbml-parse-ignore"
+          <div className={styles.advProBox}>
+            <div className={styles.advTextBox}>
+              <p>Líbí se Vám naše aplikace?</p>
+              <p>Prosím, sdílejte!</p>
+            </div>
+            <div className={styles.advButtonBox}>
+              <div
+                className="fb-share-button"
+                data-href="https://winemaker.cz"
+                data-layout=""
+                data-size=""
               >
                 <ImFacebook2 />
                 <span>Sdílet</span>
-              </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   )
 }
